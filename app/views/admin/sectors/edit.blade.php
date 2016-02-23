82c099d835d114a09421eda5d967474cf08a95ca
Mazebeat <diego.pintod@gmail.com>
2016-02-23 16:51:20 -0300

    0.2.6

--- app/views/admin/sectors/edit.blade.php
+++ app/views/admin/sectors/edit.blade.php
@@ -32,7 +32,6 @@
     <div class="form-group">
         {{ Form::label('descripcion_sector', 'Nombre Sector:', array('class'=>'col-md-2 control-label')) }}
         <div class="col-sm-10">
-            {{--				{{ Form::text('descripcion_sector', Input::old('descripcion_sector'), array('class'=>'form-control', 'placeholder'=>'Descripcion_sector', (!$isMy) ? 'readonly' : '' )) }}--}}
             {{ Form::text('descripcion_sector', Input::old('descripcion_sector'), array('class'=>'form-control', 'placeholder'=>'Descripcion_sector')) }}
         </div>
     </div>
